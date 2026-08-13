2257abcd71b1743101df659eb5f90a0d89cc7db6
tato6615 <69692472+tato6615@users.noreply.github.com>
2026-08-13 04:24:36 +0000

    fix: pass request to renderHomePage in /en route so category filter works

--- functions/en/index.js
+++ functions/en/index.js
@@ -1,5 +1,5 @@
 import { renderHomePage } from '../_lib/homepage.js';
 
-export async function onRequestGet({ env }) {
-  return renderHomePage(env, 'en');
+export async function onRequestGet({ env, request }) {
+  return renderHomePage(env, 'en', request);
 }
